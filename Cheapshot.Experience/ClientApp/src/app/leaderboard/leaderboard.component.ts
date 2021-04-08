3513513372328264fad11568c29092cc13055e21
itcig <kozlov.nickolay@geopallada.ru>
2021-04-09 00:19:06 +0300

    Фильтр лидерборда по городам

--- Cheapshot.Experience/ClientApp/src/app/leaderboard/leaderboard.component.ts
+++ Cheapshot.Experience/ClientApp/src/app/leaderboard/leaderboard.component.ts
@@ -13,18 +13,47 @@ export class LeaderboardComponent {
   public cities: City[];
   public font = new CheapshotFont();
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+  city: string;
 
+  m_http: HttpClient;
+  m_baseUrl: string
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this.m_http = http;
+    this.m_baseUrl = baseUrl;
     http.get<City[]>(baseUrl + 'city').subscribe(result => {
       this.cities = result;
     }, error => console.error(error));
 
-    http.get<Player[]>(baseUrl + 'top', {
-      params: new HttpParams()
-        .set("startDate", "2021-04-09")
-        .set("endDate", "2021-04-11")
+    this.load(undefined);
+
+
+  }
+
+  onChangeCity() {
+    console.log(this.city);
+    this.players = [];
+    this.load(this.city);
+  }
+
+  load(cityId: string) {
+
+
+    let params = new HttpParams()
+      .set("startDate", "2021-04-07")
+      .set("endDate", "2021-04-08");
+
+    if (cityId && cityId !== "world")
+      params = new HttpParams()
+        .set("startDate", "2021-04-07")
+        .set("endDate", "2021-04-08")
+        .set("cityId", cityId);
+
+    this.m_http.get<Player[]>(this.m_baseUrl + 'top', {
+      params
     }).subscribe(result => {
       this.players = result;
     }, error => console.error(error));
+
   }
 }
